c05023231dbc0c24e487b213048cb5b0b649e145
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-17 15:17:38 +0300

    add efstorerepository(inherited from istorerepository) cs to models folder,add code in program cs to creating service,add folder migrations with data from initial migration

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -4,7 +4,8 @@ using SportsStore.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStringS:SportsStoreConnection"])});
+builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStringS:SportsStoreConnection"]);});
+builder.Services.AddScoped<IStoreRepository,EFStoreRepository>();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
