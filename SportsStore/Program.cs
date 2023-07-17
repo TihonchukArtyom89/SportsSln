50846822f6bf2c7be5d69231b5609cf077918035
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-17 16:36:52 +0300

    add seeddata cs (to fill data an empty database) file to models folder, add code to program cs to fill sample data in database(Localdb), add to homecontroller cs output a list of products, add unit test on homecontroller for validate data for using repository,update view to output list of products from repository

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -2,13 +2,12 @@ using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStringS:SportsStoreConnection"]);});
 builder.Services.AddScoped<IStoreRepository,EFStoreRepository>();
 var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+SeedData.EnsurePopulated(app);
 app.Run();
+// reset databse in command line in SportsStore folder enter this: dotnet ef database drop --force --context StoreDbContext
