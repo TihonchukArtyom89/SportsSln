683b910c5a23b06d875b12fddd5ab3fe42cfe099
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-18 16:34:51 +0300

    add test for correct filtering products(can_filter_products) in homecontrollertests,styiling url scheme, modify pagelinktaghelper cs file in infrastructure folder with adding new property (pageurlvalues) for using new url scheme,update index view with using pageurlvalues

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -8,7 +8,10 @@ builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builde
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 var app = builder.Build();
 app.UseStaticFiles();
-app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", Action = "Index" });
+app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new { Controller = "Home", Action = "Index" });
+app.MapControllerRoute("page", "Page{productPage:int}", new { Controller = "Home", Action = "Index", productPage = 1 });
+app.MapControllerRoute("category", "{category}", new { Controller = "Home", Action = "Index", productPage = 1 });
+app.MapControllerRoute("pagination", "Products/Page{productPage}", new { Controller = "Home", Action = "Index", productPage = 1 });
 app.MapDefaultControllerRoute();
 SeedData.EnsurePopulated(app);
 app.Run();
