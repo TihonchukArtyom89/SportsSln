bc401e3c2b7888df094a47033d691e29d538dfed
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-21 19:59:27 +0300

    expand istorerepository cs and efstorerepository cs with add them create,delete and save items abilities in models folder,add data validation to product cs in models folder,add table with list of actions and actions (create,edit,read),add razor component to display details of product with actions (edit,go back ) details razor in pages/admin folder(button back not working)

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -9,4 +9,18 @@ public class EFStoreRepository : IStoreRepository
         context = cxt;
     }
     public IQueryable<Product> Products => context.Products;
+    public void CreateProduct(Product p)
+    {
+        context.Add(p);
+        context.SaveChanges();
+    }
+    public void DeleteProduct(Product p)
+    {
+        context.Remove(p);
+        context.SaveChanges();
+    }
+    public void SaveProduct(Product p)
+    {
+        context.SaveChanges();
+    }
 }
