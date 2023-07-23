bd5b3579ff7972e80ffee2084d6d0032c3df26da
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-23 18:53:47 +0300

    add db context class for identity database (appidentitydbcontext cs) in models folder,add connection string for identity database in appsettings json, configuring application for using identity in program cs file, add and update new migration to new database (identity),add seed data to populate identity database (identityseeddata cs) in models folder, enable seed data populating for identity database in program cs

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); });
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
-builder.Services.AddScoped<IOrderRepository,EFOrderRepository>();
+builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(sp => SessionCart.GetCat(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<AppIdentityDbContext>(opts => { opts.UseSqlServer(builder.Configuration["ConnectionStrings:IdentityConnection"]); });
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllerRoute("catpage", "{category}/Page{productPage:int}", new { Controller = "Home", Action = "Index" });
 app.MapControllerRoute("page", "Page{productPage:int}", new { Controller = "Home", Action = "Index", productPage = 1 });
 app.MapControllerRoute("category", "{category}", new { Controller = "Home", Action = "Index", productPage = 1 });
@@ -25,6 +30,7 @@ app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
 SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 app.Run();
 //if troubles with launch project (same objects in database)
 // reset databse in command line in SportsStore folder enter this: dotnet ef database drop --force --context StoreDbContext  
